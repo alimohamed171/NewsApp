8ff48bfcc3986a65697e7caa0401fd569be1d051
ALi <alimohammed1712003@gmail.com>
2024-01-21 18:26:29 +0200

    HeadlinesFragment.kt

--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
     companion object{
         const val API_KEY = "8e7b06daaec443ff971c20d140c1f681"
         const val BASE_URL = "https://newsapi.org/"
-        const val SEARCH_TIME_DELAY = "500L"
-        const val QUERY_PAGE_SIZE = "20"
+        const val SEARCH_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
